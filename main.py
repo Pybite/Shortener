43560993e9af96430f49d1a027adfd813e992045
Javed Muckett <javedmuckett@gmail.com>
2024-08-26 14:29:39 -0400

    basic backend functionality

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST','GET'])
-def home():
-    pass
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/new', methods=['POST', 'GET'])
+def url_short():
+    if request.method == 'POST':
+        url = request.form
+        print(url['long_url'])
+        return redirect('/', code=200)
+
+    else:
+        return render_template('index.html')
+    
+
+if __name__ == '__main__':
+    app.run('localhost', 5000, debug=True)
